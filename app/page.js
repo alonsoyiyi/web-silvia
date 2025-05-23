1c50b37fc3e841a5dd92228101ac8ecf57f50e94
alonsoyiyi <angel.paz.consultas@gmail.com>
2025-05-23 15:04:06 -0500

    video central

--- app/page.js
+++ app/page.js
@@ -4,12 +4,23 @@ import './components/Accordion/styles.css';
 export default function Home() {
   return (
     <main className="main-container">
-      <header className="header">
-        <h1>Proyecto Transmedial</h1>
-      </header>
-      <section className="accordion-section">
-        <Accordion />
-      </section>
+      <div className="video-section">
+        <video
+          autoPlay
+          muted
+          loop
+          playsInline
+          className="background-video"
+        >
+          <source src="/videos/fondo1.mp4" type="video/mp4" />
+        </video>
+      </div>
+
+      <div className="content-section">
+        <section className="accordion-section">
+          <Accordion />
+        </section>
+      </div>
     </main>
   );
 }
